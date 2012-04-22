85a7b39f6ec4cedfae744bf67aa4ec57de093027
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-22 21:34:03 +0900

    add bin_tree

--- lib/rbindkeys/bind_tree.rb
+++ lib/rbindkeys/bind_tree.rb
@@ -1,1 +1,64 @@
 # -*- coding:utf-8; mode:ruby; -*-
+
+module Rbindkeys
+  class BindTree
+
+    # a tree structure which that nodes are Fixnum(keycode) and
+    # leaves are Array of Fixnum(keycode)
+    attr_reader :tree
+
+    def initialize
+      @tree = {}
+    end
+
+    # register an input-output pair
+    # _input_: Array of input keycodes
+    # _output_: Array of send keycodes or Proc
+    def bind input, output
+      input = input.clone
+      tail_code = input.pop
+
+      subtree = @tree
+      last_code = -1
+      input.each do |code|
+        if subtree.has_key? code and (not subtree[code].kind_of? Hash)
+          raise DuplicateNodeError, "already register an input:#{input}"
+        elsif last_code >= code
+          raise ArgumentError, "expect a sorted Array as input"
+        end
+        subtree[code] ||= {}
+        subtree = subtree[code]
+        last_code = code
+      end
+
+      if subtree.has_key? tail_code
+        raise DuplicateNodeError, "already register an input:#{input}"
+      end
+      subtree[tail_code] = output
+    end
+
+    def resolve input
+      input = input.clone
+      tail_code = input.pop
+
+      subtree = @tree
+      last_code = -1
+      input.each do |code|
+        if last_code >= code
+          raise ArgumentError, "expect a sorted Array as input"
+        end
+        last_code = code
+        subtree = subtree[code]
+        break if subtree.nil?
+      end
+
+      if subtree.nil?
+        return nil
+      else
+        return subtree[tail_code]
+      end
+    end
+
+    class DuplicateNodeError < ArgumentError; end
+  end
+end
---
+++ spec/bind_tree_spec.rb
@@ -0,0 +1,87 @@
+# -*- coding:utf-8; mode:ruby; -*-
+
+require 'rbindkeys'
+
+include Rbindkeys
+
+describe BindTree do
+  context "normal" do
+    describe 'BindTree#bind' do
+      before do
+        @bt = BindTree.new
+      end
+
+      context 'with valid input/output' do
+        it "should construct expected tree" do
+          input = [1,2]
+          @bt.bind input, [2,3]
+          input.should == [1,2] # should not destroyable
+          @bt.bind [1,0], [2,3]
+          @bt.bind [2], [2,4]
+          @bt.tree.should == {1=>{2=>[2,3],0=>[2,3]}, 2=>[2,4]}
+        end
+      end
+      context 'with duplicate node input' do
+        it "should raise DuplicateNodeError" do
+          @bt.bind [1,2], [2,3]
+          begin
+            @bt.bind [1,2], [2,4]
+            violated "should raise"
+          rescue => e
+            e.class.should == BindTree::DuplicateNodeError
+          end
+        end
+        it "should raise DuplicateNodeError" do
+          @bt.bind [1,2], [2,3]
+          begin
+            @bt.bind [1,2,6], [2,4]
+            violated "should raise"
+          rescue => e
+            e.class.should == BindTree::DuplicateNodeError
+          end
+        end
+      end
+      context "with unsorted input" do
+        it "should raise ArgumentError" do
+          begin
+            @bt.bind [3,2,6], [2,4]
+            violated "should raise"
+          rescue => e
+            e.class.should == ArgumentError
+          end
+        end
+      end
+    end # describe
+    describe "BindTree#resolve" do
+      before do
+        @bt = BindTree.new
+        @bt.bind [1,2,3], [4,5,6]
+        @bt.bind [1,2,0], [6]
+        @bt.bind [1,3], [5,6]
+        @bt.bind [1,1], lambda{}
+        @bt.bind [2], [2]
+      end
+      context "with unsorted input" do
+        it "should rase" do
+          begin
+            @bt.resolve([2, 1, 3]).should
+            violated "should raise"
+          rescue => e
+          end
+        end
+      end
+      context "with binded input" do
+        it "should return Arrays" do
+          @bt.resolve([2]).should == [2]
+          @bt.resolve([1,2,0]).should == [6]
+        end
+      end
+      context "with no binded input" do
+        it "should return nil" do
+          @bt.resolve([4]).should be_nil
+          @bt.resolve([1,4]).should be_nil
+        end
+      end
+    end
+  end # context
+end # describe
