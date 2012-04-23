1b3e48147afa3bf9c4e4d74a92e2dd915000f8a6
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-24 00:19:14 +0900

    add skelton

---
+++ lib/rbindkeys/bind_set.rb
@@ -0,0 +1,10 @@
+# -*- coding:utf-8; mode:ruby; -*-
+
+module Rbindkeys
+
+  # BindSet is a implementation for replace with BindTree
+  # using Array#& (set intersection).
+  # because BindTree drop bind order infomation.
+  class BindSet
+  end
+end
