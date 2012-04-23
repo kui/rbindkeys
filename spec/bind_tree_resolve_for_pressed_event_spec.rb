917323c0a9f3751f98143fdb073e6a0621d5bbf3
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-24 00:47:06 +0900

    break bind_tree_spec.rb

--- spec/bind_tree_resolve_for_pressed_event_spec.rb
+++ spec/bind_tree_resolve_for_pressed_event_spec.rb
@@ -5,55 +5,6 @@ require 'rbindkeys'
 include Rbindkeys
 
 describe BindTree do
-  context "normal" do
-    describe 'BindTree#bind' do
-      before do
-        @bt = BindTree.new
-      end
-
-      context 'with valid input/output' do
-        it "should construct expected tree" do
-          input = [1,2]
-          @bt.bind input, [2,3]
-          input.should == [1,2] # should not destroyable
-          @bt.bind [1,0], [2,3]
-          @bt.bind [2], [2,4]
-          @bt.tree.should == {1=>{2=>[2,3],0=>[2,3]}, 2=>[2,4]}
-        end
-      end
-      context 'with duplicate node input' do
-        it "should raise DuplicateNodeError" do
-          @bt.bind [1,2], [2,3]
-          begin
-            @bt.bind [1,2], [2,4]
-            violated "should raise"
-          rescue => e
-            e.class.should == BindTree::DuplicateNodeError
-          end
-        end
-        it "should raise DuplicateNodeError" do
-          @bt.bind [1,2], [2,3]
-          begin
-            @bt.bind [1,2,6], [2,4]
-            violated "should raise"
-          rescue => e
-            e.class.should == BindTree::DuplicateNodeError
-          end
-        end
-      end
-      context "with unsorted input" do
-        it "should raise ArgumentError" do
-          begin
-            @bt.bind [3,2,6], [2,4]
-            violated "should raise"
-          rescue => e
-            e.class.should == ArgumentError
-          end
-        end
-      end
-    end # describe
-  end
-
   context "BindTree constracted" do
     before do
       @bt = BindTree.new
