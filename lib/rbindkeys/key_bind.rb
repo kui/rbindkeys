41c291e77f116c05a4877160d3b137983c70a842
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-24 22:33:05 +0900

    implement key_bind_class

--- lib/rbindkeys/key_bind.rb
+++ lib/rbindkeys/key_bind.rb
@@ -2,5 +2,16 @@
 
 module Rbindkeys
   class KeyBind
+
+    attr_reader :input
+    attr_reader :output
+
+    def initialize input, output
+      raise ArgumentError, "input expected as Array" if not input.kind_of? Array
+      raise ArgumentError, "output expected as Array" if not output.kind_of? Array
+
+      @input = input
+      @output = output
+    end
   end
 end
