e4245fec5b22d20382b35080bc7f11d5b5fe34b6
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-25 01:33:14 +0900

    add emacs keybindings

--- sample/swap_left_ctrl_and_caps.rb
+++ sample/swap_left_ctrl_and_caps.rb
@@ -6,7 +6,18 @@
 pre_bind_key KEY_CAPSLOCK, KEY_LEFTCTRL
 pre_bind_key KEY_LEFTCTRL, KEY_CAPSLOCK
 
-bind_key [KEY_CAPSLOCK, KEY_F], KEY_RIGHT
+bind_key [KEY_LEFTCTRL, KEY_F], KEY_RIGHT
+bind_key [KEY_LEFTCTRL, KEY_B], KEY_LEFT
+bind_key [KEY_LEFTCTRL, KEY_P], KEY_UP
+bind_key [KEY_LEFTCTRL, KEY_N], KEY_DOWN
+bind_key [KEY_LEFTCTRL, KEY_A], KEY_HOME
+bind_key [KEY_LEFTCTRL, KEY_E], KEY_END
+bind_key [KEY_LEFTCTRL, KEY_V], KEY_PAGEDOWN
+bind_key [KEY_LEFTALT, KEY_V], KEY_PAGEUP
+bind_key [KEY_LEFTCTRL, KEY_D], KEY_DELETE
+bind_key [KEY_LEFTCTRL, KEY_H], KEY_BACKSPACE
+bind_key [KEY_LEFTCTRL, KEY_M], KEY_ENTER
+bind_key [KEY_LEFTCTRL, KEY_I], KEY_TAB
 
 # bind_event KEY_LEFTCTRL do |event, context|
 #   led LED_CAPSL, event.value
