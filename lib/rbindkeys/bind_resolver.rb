f1a04a14910dcd4cda298c7eacd492bdf3df1794
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-29 14:52:50 +0900

    add ivs

--- lib/rbindkeys/bind_resolver.rb
+++ lib/rbindkeys/bind_resolver.rb
@@ -3,6 +3,18 @@
 module Rbindkeys
 
   class BindResolver
+    DEFAULT_DEFAULT_VALUE = :through
+
+    attr_reader :tree
+
+    # returned value if no binds hit
+    attr_reader :default_value
+
+    def initialize default_value = DEFAULT_DEFAULT_VALUE
+      @tree = {}
+      @default_value = default_value
+    end
+
   end
 
 end
