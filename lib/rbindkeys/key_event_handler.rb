e751348c90732d94cef9bb4f1da3a23bd4bd7070
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-01 18:23:44 +0900

    add bind_prefix_key for 2 stroke

--- lib/rbindkeys/key_event_handler.rb
+++ lib/rbindkeys/key_event_handler.rb
@@ -4,6 +4,7 @@ module Rbindkeys
 
   # retrive key binds with key event
   class KeyEventHandler
+    include Revdev
 
     LOG = LogUtils.get_logger name
 
@@ -19,6 +20,9 @@ module Rbindkeys
     # current key bind set which retrive key binds with a key event
     attr_reader :bind_resolver
 
+    #  proccessed resolver before bind_resolver
+    attr_reader :pre_bind_resolver
+
     # code set of pressed key on the event device
     attr_reader :pressed_key_set
 
@@ -33,6 +37,7 @@ module Rbindkeys
       @virtual = virtual_dev
       @default_bind_resolver = BindResolver.new
       @bind_resolver = @default_bind_resolver
+      @pre_bind_resolver = {}
       @pressed_key_set = []
       @virtual_pressed_key_set = []
       @active_bind_set = []
@@ -41,6 +46,73 @@ module Rbindkeys
     def load_config file
       code = File.read file
       instance_eval code, file
+    end
+
+    def pre_bind_key input, output
+      if not (input.kind_of? Fixnum and output.kind_of? Fixnum)
+        raise ArgumentError, 'expect Fixnum for input and output'
+      end
+
+      if @pre_bind_resolver.has_key? input
+        raise DuplicateNodeError, "1st arg (#{input}) was already entried"
+      end
+
+      LOG.info "pre_bind_key #{input.inspect},\t#{output.inspect}"
+
+      @pre_bind_resolver[input] = output
+      [input, output]
+    end
+
+    def bind_key input, output, resolver=@bind_resolver
+      input = parse_code input
+      output = parse_code output
+
+      LOG.info "bind_key #{input.inspect},\t#{output.inspect}\t#{resolver}"
+
+      resolver.bind input, output
+    end
+
+    def bind_prefix_key input, resolver=@bind_resolver
+      if not block_given?
+        raise ArgumentError, "expect to a block"
+      end
+
+      input = parse_code input.clone
+      tail_input = input.pop
+
+      binded_resolver = @bind_resolver.resolve tail_input, input
+      if binded_resolver == @bind_resolver.default_value
+        binded_resolver = BindResolver.new
+        resolver.bind input, binded_resolver
+      end
+
+      @bind_resolver = binded_resolver
+      yield
+      @binded_resolver = resolver
+      binded_resolver
+    end
+
+    # parse and normalize to Fixnum/Array
+    def parse_code code, depth = 0
+      if code.kind_of? Symbol
+        code = parse_symbol code
+      elsif code.kind_of? Array
+        raise ArgumentError, "expect Array is the depth less than 1" if depth >= 1
+        code.map!{|c| parse_code c, (depth+1)}
+      elsif code.kind_of? Fixnum and depth == 0
+        code = [code]
+      elsif not code.kind_of? Fixnum
+        raise ArgumentError, "expect Symbol / Fixnum / Array"
+      end
+      code
+    end
+
+    # TODO convert :j -> KEY_J, :ctrl -> KEY_LEFTCTRL
+    def parse_symbol sym
+      if not sym.kind_of? Symbol
+        raise ArgumentError, "expect Symbol / Fixnum / Array"
+      end
+      Revdev.const_get sym
     end
 
   end
--- spec/key_event_handler_load_config_spec.rb
+++ spec/key_event_handler_load_config_spec.rb
@@ -2,12 +2,13 @@
 
 require 'rbindkeys'
 require 'fileutils'
+require 'revdev'
 
 include Rbindkeys
 
 $tmp_dir = 'tmp'
 
-describe Observer do
+describe KeyEventHandler do
   before :all do
     FileUtils.mkdir $tmp_dir
   end
@@ -21,7 +22,50 @@ describe Observer do
     @vdev = mock(Rbindkeys::VirtualDevice)
   end
 
-  context "simple keybinds" do
+  describe "#bind_prefix_key" do
+    before do
+      @defval = :hoge
+      @bind_set = []
+      res = mock Rbindkeys::BindResolver
+
+      # define stubs
+      res.stub(:default_value) { @defval }
+      res.stub(:bind) do |i, o|
+        @bind_set << [i, o]
+      end
+      res.stub(:resolve) do |input, pressed_key_set|
+        if input == 10
+          BindResolver.new
+        else
+          @defval
+        end
+      end
+      Rbindkeys::BindResolver.stub!(:new).and_return(res)
+
+      @handler = KeyEventHandler.new @dev, @vdev
+    end
+
+    context "add a new prefix key" do
+      it "construct @bind_set" do
+        @handler.bind_prefix_key [0,1] do
+          @handler.bind_key 2, 3
+        end
+        @bind_set.length.should == 2
+        @bind_set.include?([[2],[3]]).should be_true
+      end
+    end
+    context "add a existing prefix key" do
+      it "construct @bind_set" do
+        @handler.bind_prefix_key [0,10] do
+          @handler.bind_key 2, 3
+        end
+        @bind_set.length.should == 1
+        @bind_set.include?([[2],[3]]).should be_true
+      end
+    end
+  end
+
+  describe "KeyEventHandler#load_config" do
     $config = File.join($tmp_dir, 'config.rb')
 
     before :all do
@@ -30,20 +74,29 @@ describe Observer do
 pre_bind_key KEY_CAPSLOCK, KEY_LEFTCTRL
 bind_key [KEY_LEFTCTRL,KEY_F], KEY_RIGHT
 bind_key [KEY_LEFTCTRL,KEY_W], [KEY_LEFTCTRL,KEY_X]
+bind_prefix_key [KEY_LEFTCTRL,KEY_X] do
+  bind_key KEY_K, [KEY_LEFTCTRL, KEY_W]
+end
 EOF
       end
     end
 
     before do
+      @bind_set = []
+      res = mock Rbindkeys::BindResolver
+      res.stub(:bind) do |i, o|
+        @bind_set << [i, o]
+      end
+      Rbindkeys::BindResolver.stub!(:new).and_return(res)
       @handler = KeyEventHandler.new @dev, @vdev
     end
 
-    describe "KeyEventHandler#load_config" do
-      it "construct @pre_bind_key_set and @bind_key_set" do
-        @handler.load_config $config
-        @handler.pre_bind_key.size.should == 1
-        @handler.pre_bind_key.size.should == 2
-      end
+    it "construct @pre_bind_key_set and @bind_key_set" do
+      @handler.load_config $config
+      @handler.pre_bind_resolver.size.should == 1
+      @bind_set.length.should == 3
+      @bind_set[0][1].should == [Revdev::KEY_RIGHT]
+      p @bind_set
     end
   end
 end
