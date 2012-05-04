298047cf1a0c150ef7797a8c9697dcdaf586bd3a
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-04 17:47:18 +0900

    add a function to switch the log format

--- lib/rbindkeys/log_utils.rb
+++ lib/rbindkeys/log_utils.rb
@@ -7,11 +7,31 @@ module Rbindkeys
   class LogUtils
 
     DEFAULT_LOG_OUTPUT = STDOUT
+    DEFAULT_FORMAT = :simple
+    @@format = DEFAULT_FORMAT
 
-    def LogUtils.get_logger progname
-      logger = Logger.new DEFAULT_LOG_OUTPUT
-      logger.progname = progname
-      logger
+    class << self
+
+      def get_logger progname
+        logger = Logger.new DEFAULT_LOG_OUTPUT
+        logger.progname = progname
+        set_formatter logger
+        logger
+      end
+
+      def set_formatter logger
+        case @@format
+        when :simple
+          logger.formatter = proc do |sev, date, prog, msg|
+            "#{msg}\n"
+          end
+        when :default
+        else
+          l = Logger.new STDERR
+          l.fatal "unknown logger format"
+          exit false
+        end
+      end
     end
 
   end
