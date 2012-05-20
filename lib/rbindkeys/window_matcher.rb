bd370013f1771786e5ed9778602a6fb172e5729c
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-20 22:31:24 +0900

    fix bug

--- lib/rbindkeys/window_matcher.rb
+++ lib/rbindkeys/window_matcher.rb
@@ -6,21 +6,29 @@
 module Rbindkeys
   class WindowMatcher
 
+    attr_reader :app_name, :title
+
     def initialize h
       @app_name = (h[:class] or h[:app_name] or h[:app_class])
       @title = (h[:title] or h[:name])
+
+      if not @app_name.nil? and not @title.nil?
+        raise ArgumentError, 'expect to be given :class, :app_name,'+
+          ' :app_class, :title or :name '
+      end
     end
 
     def match? app_name, title
-        match_app?(app_name) and match_title?(title)
+      (@app_name.nil? or match_app?(app_name)) and
+        (@title.nil? or match_title?(title))
     end
 
     def match_app? app_name
-      app_name and @app_name and app_name.match @app_name
+      app_name and app_name.match @app_name
     end
 
     def match_title? title
-      title and @title and title.match @title
+      title and title.match @title
     end
 
   end
