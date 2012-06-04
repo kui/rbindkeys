dad95956ab64f968b2fd51d41d6e331a6d52f0a7
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-06-04 23:33:46 +0900

    add cli.rb

--- lib/rbindkeys.rb
+++ lib/rbindkeys.rb
@@ -1,20 +1,22 @@
 # -*- coding:undecided-unix; mode:ruby; -*-
 
-require "rubygems"
-require "rbindkeys/version"
-require "rbindkeys/log_utils"
+require 'rubygems'
+require 'rbindkeys/version'
+require 'rbindkeys/log_utils'
 
-require "rbindkeys/key_bind"
-require "rbindkeys/bind_tree"
-require "rbindkeys/observer"
-require "rbindkeys/device"
-require "rbindkeys/virtual_device"
+require 'rbindkeys/key_bind'
+require 'rbindkeys/bind_tree'
+require 'rbindkeys/observer'
+require 'rbindkeys/device'
+require 'rbindkeys/virtual_device'
 
-require "rbindkeys/device_operator"
-require "rbindkeys/key_event_handler"
-require "rbindkeys/window_matcher"
-require "rbindkeys/bind_resolver"
-require "rbindkeys/fix_resolver"
+require 'rbindkeys/device_operator'
+require 'rbindkeys/key_event_handler'
+require 'rbindkeys/window_matcher'
+require 'rbindkeys/bind_resolver'
+require 'rbindkeys/fix_resolver'
+
+require 'rbindkeys/cli'
 
 module Rbindkeys
 
@@ -28,6 +30,8 @@ module Rbindkeys
   class KeyEventHandler; end
   class BindResolver; end
   class FixResolver; end
+
+  class CLI; end
 
   class DuplicateNodeError < ArgumentError; end
   class UnknownKeyValue < Exception; end
--- lib/rbindkeys/observer.rb
+++ lib/rbindkeys/observer.rb
@@ -1,6 +1,5 @@
 # -*- coding:utf-8; mode:ruby; -*-
 
-require "thread"
 require "revdev"
 
 module Rbindkeys
